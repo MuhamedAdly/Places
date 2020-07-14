12f3dc0240fd577bf79043e97da31a8c579dacf4
Mohamed Adly <muhamedadly@gmail.com>
2020-07-14 08:18:34 +0200

    Initial Commit

---
+++ Places/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Places
+//
+//  Created by Mohamed Adly on 7/14/20.
+//  Copyright © 2020 Mohamed Adly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
